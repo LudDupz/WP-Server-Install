aea5ddda948fc2f116d22ae95b8ad62d4bbb0f1e
LudDupz <luddupz@gmail.com>
2021-08-19 17:38:17 +0200

    Test Install

---
+++ bin/_configuration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MYSQL_USER="explorateur"
+MYSQL_PASSWORD="Ereul9Aeng"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="getgigback"
+WORDPRESS_URL="ec2-54-146-132-182.compute-1.amazonaws.com/getgig-back/public"
+WORDPRESS_SITE_NAME="GETGIG"
+WORDPRESS_ADMIN_NAME="getgigback"
+WORDPRESS_ADMIN_PASSWORD="anTOpaLU9659$"
+WORDPRESS_ADMIN_EMAIL="ampat.ae@gmail.com"
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="wpsh_"
